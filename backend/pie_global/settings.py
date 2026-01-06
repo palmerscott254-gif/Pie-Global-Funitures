c6ba5fe1dc3959771a0873b4a03c15b866746297
Scott Newton <palmerscott254@gmail.com>
2026-01-06 19:25:52 +0300

    pip

--- backend/pie_global/settings.py
+++ backend/pie_global/settings.py
@@ -230,7 +230,10 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='pieglobal308@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='pieglobal308@gmail.com')
 
-# Logging
+# Logging - Create logs directory if it doesn't exist
+LOGS_DIR = BASE_DIR / 'logs'
+LOGS_DIR.mkdir(exist_ok=True)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -247,7 +250,7 @@ LOGGING = {
         },
         'file': {
             'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
+            'filename': LOGS_DIR / 'django.log',
             'formatter': 'verbose',
         },
     },
